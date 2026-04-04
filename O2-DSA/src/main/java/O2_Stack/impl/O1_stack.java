2f551197fc170bd32a599eabf17735c81ffe1663
prajul-shinde <prajulshinde@gmail.com>
2026-04-04 10:09:42 +0530

    stack implementation

---
+++ O2-DSA/src/main/java/O2_Stack/impl/O1_stack.java
@@ -0,0 +1,57 @@
+import static java.lang.IO.println;
+
+class ArrayStack {
+
+    private ArrayList<Integer> stack;
+
+    public ArrayStack() {
+        this.stack = new ArrayList<>();
+    }
+
+    public void push(int n) {
+        stack.add(n);
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public int peek() {
+        if (isEmpty())
+            return -1;
+        return stack.get(size() - 1);
+    }
+
+    private boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public int pop() {
+        if (isEmpty())
+            return -1;
+        return stack.remove(size() - 1);
+    }
+}
+
+void main() {
+    var myStack = new ArrayStack();
+
+    System.out.println("1. Pushing 10, 20, 30...");
+    myStack.push(10);
+    myStack.push(20);
+    myStack.push(30);
+
+    println("Current Size: " + myStack.size());
+
+    println("\n2. Peeking at the top...");
+    println("Top Element (Peek): " + myStack.peek()); // Should be 30
+    println("Notice size is still: " + myStack.size() + " (Peek doesn't remove it)");
+
+    println("\n3. Popping elements...");
+    println("Popped: " + myStack.pop()); // Removes 30
+    println("Popped: " + myStack.pop()); // Removes 20
+
+    println("\n4. Final State Check");
+    println("Current Size: " + myStack.size()); // Should be 1
+    println("Top Element is now: " + myStack.peek()); // Should be 10
+}
